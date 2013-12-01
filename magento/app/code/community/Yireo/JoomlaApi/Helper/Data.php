95aa3732ba866e027bb2b5de88fe1ff1e6569f42
Jisse Reitsma <info@yireo.com>
2013-12-01 14:43:50 +0100

    Add JoomlaApi module to core

---
+++ magento/app/code/community/Yireo/JoomlaApi/Helper/Data.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * JoomlaApi
+ *
+ * @author Yireo
+ * @package JoomlaApi
+ * @copyright Copyright 2013
+ * @license Open Source License v3
+ * @link http://www.yireo.com
+ */
+
+class Yireo_JoomlaApi_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    /*
+     * Helper-method to return the Joomla! path
+     *
+     * @access public
+     * @param null
+     * @return bool
+     */
+    public function getJoomlaPath()
+    {
+        return Mage::getStoreConfig('joomlaapi/settings/path');
+    }
+}
