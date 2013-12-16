64dadfa6e954cca39888690269aec223c515d302
Jisse Reitsma <info@yireo.com>
2013-12-16 16:31:13 +0100

    When bridging all cookies, make sure to add them to the getBridgableCookies() call

--- joomla2/components/com_magebridge/helpers/bridge.php
+++ joomla2/components/com_magebridge/helpers/bridge.php
@@ -17,8 +17,29 @@ defined('_JEXEC') or die('Restricted access');
  */
 class MageBridgeBridgeHelper
 {
+    /*
+     * Method to return all cookies that are allowed to pass between Joomla! and Magento
+     *
+     * @access public
+     * @static
+     * @param null
+     * @return array
+     */
     static public function getBridgableCookies()
     {
+        // When bridging all cookies, simply collect all names and use them
+        $allCookies = MagebridgeModelConfig::load('bridge_cookie_all');
+        if($allCookies == 1 && !empty($_COOKIE)) {
+            $cookies = array();
+            foreach($_COOKIE as $cookieName => $cookieValue) {
+                if(preg_match('/^__ut/', $cookieName)) continue;
+                if(preg_match('/^PHPSESSID/', $cookieName)) continue;
+                $cookies[] = $cookieName;
+            }
+            return $cookies;
+        }
+
+        // Otherwise define a default list of cookies
         $application = JFactory::getApplication();
         if ($application->isSite() == 1 ) {
             $cookies = array('frontend', 'user_allowed_save_cookie', 'persistent_shopping_cart');
@@ -26,6 +47,7 @@ class MageBridgeBridgeHelper
             $cookies = array('admin');
         }
 
+        // Add the custom cookies to the default list
         $extraCookies = MagebridgeModelConfig::load('bridge_cookie_custom');
         if(!empty($extraCookies)) {
             $extraCookies = explode(',', $extraCookies);
