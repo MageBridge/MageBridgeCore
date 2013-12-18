1effafb4bca8d4980a3634c78da7bbe427e0ac22
Jisse Reitsma <info@yireo.com>
2013-12-18 18:44:45 +0100

    Add example MageBridge Newsletter Plugin

---
+++ joomla2/plugins/magebridge.newsletter/example/example.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * MageBridge Newsletter plugin - Example
+ *
+ * @author Yireo (info@yireo.com)
+ * @package MageBridge
+ * @copyright Copyright 2013
+ * @license GNU Public License
+ * @link http://www.yireo.com
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
+// Import the MageBridge autoloader
+require_once JPATH_SITE.'/components/com_magebridge/helpers/loader.php';
+
+/**
+ * MageBridge Newsletter Plugin - Example
+ */
+class plgMageBridgeNewsletterExample extends MageBridgePluginMagento
+{
+    /**
+     * Event "onNewsletterSubscribe"
+     * 
+     * @access private
+     * @param object $user Joomla! user object
+     * @param tinyint $state Whether the user is subscribed or not (0 for no, 1 for yes)
+     * @return bool
+     */
+    public function onNewsletterSubscribe($user, $state)
+    {
+        // Make sure this plugin is enabled
+        if ($this->isEnabled() == false) {
+            return false;
+        }
+
+        // Do your stuff to subscribe an user to a specific newsletter
+
+        return true;
+    }
+
+    /*
+     * Method to check whether this plugin is enabled or not
+     *
+     * @param null
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        // Check for the existance of a specific component
+        return $this->checkComponent('com_example');
+    }
+}
+
