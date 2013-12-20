0d02331544c2d64c49e0dcc899752ddfb1d03102
Jisse Reitsma <info@yireo.com>
2013-12-20 11:08:53 +0100

    Update copyright

--- joomla2/components/com_magebridge/helpers/loader.php
+++ joomla2/components/com_magebridge/helpers/loader.php
@@ -4,7 +4,7 @@
  *
  * @author Yireo (info@yireo.com)
  * @package MageBridge
- * @copyright Copyright 2011
+ * @copyright Copyright 2013
  * @license GNU Public License
  * @link http://www.yireo.com
  */
