ca310002daba277a2c91a6ba83f08fe7566dd175
Jisse Reitsma <info@yireo.com>
2014-02-28 07:39:31 +0100

    Rewrite Store Relation form to JForm and new plugin arch

---
+++ joomla2/administrator/components/com_magebridge/views/store/tmpl/actions.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+ * Joomla! component MageBridge
+ *
+ * @author Yireo (info@yireo.com)
+ * @package MageBridge
+ * @copyright Copyright 2014
+ * @license GNU Public License
+ * @link http://www.yireo.com
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+$form = $this->actions_form;
+$fieldCount = count($form->getFieldset('actions'));
+?>
+<?php if($fieldCount > 0) : ?>
+<table class="admintable">
+<?php foreach($form->getFieldset('actions') as $field): ?>
+    <tr>
+        <td class="key"><?php echo $field->label; ?></td>
+        <td class="value"><?php echo $field->input; ?></td>
+    </tr>
+<?php endforeach; ?>
+</table>
+<?php else: ?>
+<p><?php echo JText::_('COM_MAGEBRIDGE_STORE_NO_PLUGINS'); ?></p>
+<?php endif; ?>
