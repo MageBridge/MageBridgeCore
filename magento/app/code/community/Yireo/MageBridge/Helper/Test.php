f443a2bd357ed62b37ea3fc58e3da04666e51ca0
Jisse Reitsma <info@yireo.com>
2014-07-18 10:33:49 +0200

    Add simple test-helper for developer purpose

---
+++ magento/app/code/community/Yireo/MageBridge/Helper/Test.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * MageBridge
+ *
+ * @author Yireo
+ * @package MageBridge
+ * @copyright Copyright 2014
+ * @license Open Source License
+ * @link http://www.yireo.com
+ */
+
+class Yireo_MageBridge_Helper_Test extends Mage_Core_Helper_Abstract
+{
+    /*
+     * Return whether the current page is seen as an internal page or not
+     *
+     * @access public
+     * @param null
+     * @return bool
+     */
+    public function isInternalPage()
+    {
+        $url = Mage::getSingleton('magebridge/core')->getMetaData('joomla_current_url');
+        if (strpos($url, 'http') !== false) {
+            if ((strpos($url, Mage::app()->getStore()->getBaseUrl()) === 0)
+                || (strpos($url, Mage::app()->getStore()->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK, true)) === 0)
+            ) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
