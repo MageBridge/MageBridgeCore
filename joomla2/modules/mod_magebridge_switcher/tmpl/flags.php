d7428ad75b3f47b72b3debe0f0834a26ef25d4ea
Jisse Reitsma <info@yireo.com>
2014-08-10 10:17:57 +0200

    Add flags layout to MageBridge Store Switcher

---
+++ joomla2/modules/mod_magebridge_switcher/tmpl/flags.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Joomla! module MageBridge: Shopping Cart
+ *
+ * @author Yireo (info@yireo.com)
+ * @package MageBridge
+ * @copyright Copyright 2014
+ * @license GNU Public License
+ * @link http://www.yireo.com
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+?>
+<div id="magebridge-switcher" class="magebridge-module mod-languages">
+    <ul class="lang-inline">
+        <?php foreach($languages as $language) : ?>
+        <?php $image = '/media/mod_languages/images/'.$language['code'].'.gif'; ?>
+        <?php $label = $language['label'] ; ?>
+        <?php $url = $language['url']; ?>
+        <li class="lang-active" dir="ltr">
+            <a href="<?php echo $url; ?>"><img src="<?php echo $image; ?>" alt="<?php echo $label; ?>" title="<?php echo $label; ?>" /></a>
+        </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
