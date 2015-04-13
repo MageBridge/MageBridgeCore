a68211dd2570cb019df100d9884df2f115e0005a
Jisse Reitsma <info@yireo.com>
2015-04-13 21:09:46 +0200

    Pending changes

---
+++ source/libraries/yireo/model/data.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Joomla! Yireo Library
+ *
+ * @author Yireo (http://www.yireo.com/)
+ * @package YireoLib
+ * @copyright Copyright 2015
+ * @license GNU Public License
+ * @link http://www.yireo.com/
+ * @version 0.6.0
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+
+// Import the loader
+require_once dirname(dirname(__FILE__)).'/loader.php';
+
+/**
+ * Yireo Data Model
+ *
+ * @package Yireo
+ */
+class YireoDataModel extends YireoModel
+{
+}
