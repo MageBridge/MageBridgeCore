7c84d1f7973c8c56c3963ab2c62098ad86c763a8
Jisse Reitsma <info@yireo.com>
2015-09-19 14:51:21 +0200

    Pending changes

--- joomla/components/com_magebridge/magebridge.php
+++ joomla/components/com_magebridge/magebridge.php
@@ -17,28 +17,33 @@ require_once JPATH_COMPONENT.'/libraries/factory.php';
 require_once JPATH_COMPONENT.'/helpers/loader.php';
 require_once JPATH_ADMINISTRATOR.'/components/com_magebridge/libraries/loader.php';
 
+$app = JFactory::getApplication();
+$input = $app->input;
+
 // Handle the SSO redirect
-if (JFactory::getApplication()->input->getInt('sso') == 1) {
-	JFactory::getApplication()->input->set('task', 'ssoCheck');
+if ($input->getInt('sso') == 1) {
+	$input->set('task', 'ssoCheck');
 }
 
 // Handle direct proxy requests
-if (JFactory::getApplication()->input->get('url')) {
-	JFactory::getApplication()->input->set('task', 'proxy');
+if ($input->get('url')) {
+	$input->set('task', 'proxy');
 }
 
 // Initialize debugging
 MagebridgeModelDebug::init();
-			
+    
+// Simple security measure
+$input->set('task', $input->getCmd('task'));
+
 // Require the controller
-$requestedController = JFactory::getApplication()->input->getCmd('controller');
+$requestedController = $input->getCmd('controller');
+
 if ($requestedController == 'jsonrpc') {
-	JFactory::getApplication()->input->set('task', JFactory::getApplication()->input->getCmd('task', '', 'get'));
 	require_once JPATH_COMPONENT.'/controllers/default.jsonrpc.php';
 	$controller = new MageBridgeControllerJsonrpc( );
 
 } elseif ($requestedController == 'sso') {
-	JFactory::getApplication()->input->set('task', JFactory::getApplication()->input->getCmd('task', '', 'get'));
 	require_once JPATH_COMPONENT.'/controllers/default.sso.php';
 	$controller = new MageBridgeControllerSso( );
 
@@ -48,5 +53,5 @@ if ($requestedController == 'jsonrpc') {
 }
 
 // Perform the Request task
-$controller->execute( JFactory::getApplication()->input->getCmd('task'));
+$controller->execute($input->getCmd('task'));
 $controller->redirect();
