87600ba77586f73e3274d6b89b06c4ef9c6c9808
Jisse Reitsma <info@yireo.com>
2015-09-21 10:27:55 +0200

    Increase version to v1.9.5356

--- joomla/plugins/magento/magebridge/magebridge.php
+++ joomla/plugins/magento/magebridge/magebridge.php
@@ -2,32 +2,30 @@
 /**
  * Joomla! MageBridge - Magento plugin
  *
- * @author Yireo (info@yireo.com)
- * @package MageBridge
- * @copyright Copyright 2015
- * @license GNU Public License
- * @link http://www.yireo.com
+ * @author    Yireo (info@yireo.com)
+ * @package   MageBridge
+ * @copyright Copyright 2014
+ * @license   GNU Public License
+ * @link      http://www.yireo.com
  */
 
 // Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
 // Import the parent class
-jimport( 'joomla.plugin.plugin' );
-		
+jimport('joomla.plugin.plugin');
+
 // Import the MageBridge autoloader
-require_once JPATH_SITE.'/components/com_magebridge/helpers/loader.php';
+require_once JPATH_SITE . '/components/com_magebridge/helpers/loader.php';
 
 /**
  * MageBridge Magento Plugin
  */
-class plgMagentoMageBridge extends JPlugin
+class PlgMagentoMageBridge extends JPlugin
 {
 	/**
 	 * Get the MageBridge user-class
-	 * 
-	 * @access public
-	 * @param null
+	 *
 	 * @return MageBridgeModelUser
 	 */
 	public function getUser()
@@ -38,14 +36,15 @@ class plgMagentoMageBridge extends JPlugin
 	/**
 	 * Handle the event that is generated after a customer is deleted (JSON-RPC)
 	 *
-	 * @access public
 	 * @param array $arguments
+	 *
 	 * @return bool
 	 */
 	public function mageCustomerDeleteAfter($arguments = array())
 	{
 		// Abort if the input is invalid
-		if (empty($arguments) || empty($arguments['customer']['email'])) {
+		if (empty($arguments) || empty($arguments['customer']['email']))
+		{
 			return false;
 		}
 
@@ -53,7 +52,8 @@ class plgMagentoMageBridge extends JPlugin
 		$user = $this->getUser()->loadByEmail($arguments['customer']['email']);
 
 		// If there is an user with this email, delete it, but only if we are allowed to
-		if ($this->getUser()->allowSynchronization($user, 'delete')) {
+		if ($this->getUser()->allowSynchronization($user, 'delete'))
+		{
 			$user->delete();
 		}
 
@@ -62,15 +62,16 @@ class plgMagentoMageBridge extends JPlugin
 
 	/**
 	 * Handle the event that is generated after a customer logs out (bridge-response)
-	 * 
-	 * @access public
+	 *
 	 * @param array $arguments
+	 *
 	 * @return bool
 	 */
 	public function mageCustomerLogoutAfter($arguments = array())
 	{
 		// Abort if the input is invalid
-		if (empty($arguments) || empty($arguments['customer']['email'])) {
+		if (empty($arguments) || empty($arguments['customer']['email']))
+		{
 			return false;
 		}
 
@@ -81,31 +82,33 @@ class plgMagentoMageBridge extends JPlugin
 		$customer['id'] = $user->id;
 
 		// Add options for our own user-plugin
-		// @todo: New 1.6 options:
+		// @todo: options:
 		// - action = core.login.site
 		// - remember = 1
 		// - return = {URL}
 		// - entry_url = {URL}
 		$options = array('disable_bridge' => true, 'action' => 'core.login.site');
 
-		// Call the Joomla! event "onUserLogout"
+		// Call the Joomla! event "onLogoutUser"
+		$eventName = (MageBridgeHelper::isJoomla15()) ? 'onLogoutUser' : 'onUserLogout';
 		JPluginHelper::importPlugin('user');
-		JFactory::getApplication()->triggerEvent('onUserLogout', array($customer, $options));
+		JFactory::getApplication()->triggerEvent($eventName, array($customer, $options));
 
 		return true;
 	}
 
 	/**
 	 * Handle the event that is generated when a customer logs in (JSON-RPC)
-	 * 
-	 * @access public
+	 *
 	 * @param array $arguments
+	 *
 	 * @return bool
 	 */
 	public function mageCustomerLogin($arguments = array())
 	{
 		// Abort if the input is invalid
-		if (empty($arguments) || empty($arguments['customer']['email'])) {
+		if (empty($arguments) || empty($arguments['customer']['email']))
+		{
 			return false;
 		}
 
@@ -114,7 +117,8 @@ class plgMagentoMageBridge extends JPlugin
 		$user = $this->getUser()->loadByEmail($customer['email']);
 
 		// We had a succesfull login in Magento, but the user does not exist in Joomla! yet
-		if (empty($user)) {
+		if (empty($user))
+		{
 			$customer['username'] = $customer['email'];
 			$user = $this->getUser()->create($customer, true);
 		}
@@ -125,15 +129,16 @@ class plgMagentoMageBridge extends JPlugin
 
 	/**
 	 * Handle the event that is generated after a customer logs in (passed into the bridge-response)
-	 * 
-	 * @access public
+	 *
 	 * @param array $arguments
+	 *
 	 * @return bool
 	 */
 	public function mageCustomerLoginAfter($arguments = array())
 	{
 		// Abort if the input is invalid
-		if (empty($arguments) || empty($arguments['customer']['email'])) {
+		if (empty($arguments) || empty($arguments['customer']['email']))
+		{
 			return false;
 		}
 
@@ -142,22 +147,28 @@ class plgMagentoMageBridge extends JPlugin
 		$user = $this->getUser()->loadByEmail($customer['email']);
 
 		// We had a succesfull login in Magento, but the user does not exist in Joomla! yet
-		if (empty($user)) {
+		if (empty($user))
+		{
 			$customer['username'] = $customer['email'];
 			$customer['fullname'] = $customer['name'];
 			$this->getUser()->create($customer, true);
 			$user = $this->getUser()->loadByEmail($customer['email']);
 
-		} else {
+		}
+		else
+		{
 			$customer['fullname'] = $user->get('name');
 			$customer['email'] = $user->get('email');
 			$customer['username'] = $user->get('username');
 		}
 
 		// Check for the right user-ID
-		if ($user->id > 0) {
+		if ($user->id > 0)
+		{
 			$customer['id'] = $user->id;
-		} else {
+		}
+		else
+		{
 			return false;
 		}
 
@@ -169,48 +180,64 @@ class plgMagentoMageBridge extends JPlugin
 
 	/**
 	 * Handle the event that is generated after a customer is saved in Magento (JSON-RPC)
-	 * 
-	 * @access public
+	 *
 	 * @param array $arguments
+	 *
 	 * @return int
 	 */
 	public function mageCustomerSaveAfter($arguments = array())
 	{
 		// Check if this call is valid
-		if (!isset($arguments['customer'])) return false;
-		if (!isset($arguments['customer']['email'])) return false;
+		if (!isset($arguments['customer']))
+		{
+			return false;
+		}
+
+		if (!isset($arguments['customer']['email']))
+		{
+			return false;
+		}
 
 		// Fetch the right variables
 		$customer = $arguments['customer'];
-		if (isset($customer['addresses'][0][0])) {
+
+		if (isset($customer['addresses'][0][0]))
+		{
 			$address = $customer['addresses'][0][0];
 			unset($customer['addresses']);
-		} else {
+		}
+		else
+		{
 			$address = array();
 		}
 
 		// Try to load the user through the Joomla! ID stored in Magento
-		if (isset($customer['joomla_id'])) {
-			$user = JFactory::getUser($customer['joomla_id']);
+		if (isset($customer['joomla_id']))
+		{
+			$user = JFactory::getUser();
+			$user->load($customer['joomla_id']);
 		}
 
 		// Try to load the user through its email-address
-		if (!isset($user) || !isset($user->id) || !$user->id > 0) {
+		if (!isset($user) || !isset($user->id) || !$user->id > 0)
+		{
 			$user = $this->getUser()->loadByEmail($customer['email']);
 		}
 
 		// Encrypt the user-password before continuing
-		if (isset($customer['password'])) {
+		if (isset($customer['password']))
+		{
 			$customer['password'] = MageBridge::decrypt($customer['password']);
 		}
 
 		// Start building the data-input for creating the Joomla! user
-		if (!empty($customer['name']) && !empty($customer['email'])) {
-
+		if (!empty($customer['name']) && !empty($customer['email']))
+		{
 			$data = array();
 
 			// Include the received email
-			if (!empty($customer['email'])) {
+			if (!empty($customer['email']))
+			{
 				$data['email'] = $customer['email'];
 			}
 
@@ -223,21 +250,25 @@ class plgMagentoMageBridge extends JPlugin
 			// Set the firstname and lastname
 			$data['magebridgefirstlast'] = array(
 				'firstname' => $customer['firstname'],
-				'lastname' => $customer['lastname'],
-			);
+				'lastname' => $customer['lastname'],);
 
 			// Include the password
-			if (!empty($customer['password'])) {
+			if (!empty($customer['password']))
+			{
 				$data['password'] = $customer['password'];
 				$data['password2'] = $customer['password'];
 			}
 
 			// Activate this account, if it's also activated in Magento
-			if (isset($customer['is_active'])) {
-				if ($customer['is_active'] == 1) {
+			if (isset($customer['is_active']))
+			{
+				if ($customer['is_active'] == 1)
+				{
 					$data['activation'] = '';
 					$data['block'] = 0;
-				} else {
+				}
+				else
+				{
 					$data['block'] = 1;
 				}
 			}
@@ -245,41 +276,56 @@ class plgMagentoMageBridge extends JPlugin
 			// Add the usergroup ID to this user (based upon groups configured in #__magebridge_usergroups)
 			$usergroups = (is_array($user->groups)) ? $user->groups : array();
 			$customer['usergroup'] = MageBridgeUserHelper::getJoomlaGroupIds($customer, $usergroups);
-			MageBridgeModelDebug::getInstance()->trace("Customer group", $customer['usergroup']);
-			if (!empty($customer['usergroup'])) {
+			//MageBridgeModelDebug::getInstance()->trace("Customer group", $customer['usergroup']);
+
+			if (!empty($customer['usergroup']))
+			{
 				$user->groups = $customer['usergroup'];
 			}
 
 			// If this user did not exist yet, create it
-			if ($this->params->get('autocreate', 1) == 1 && ($user == false || $user->id == 0)) {
+			if ($this->params->get('autocreate', 1) == 1 && ($user == false || $user->id == 0))
+			{
 
 				$user = $this->getUser()->create($data, true);
-				if (is_object($user) && $user->id > 0) {
 
+				if (is_object($user) && $user->id > 0)
+				{
 					// Save the user through the Profile Connectors
 					$profile = new MageBridgeConnectorProfile();
 					$profile->onSave($user, $customer, $address);
 
 					// Return the new user-ID
 					return $user->id;
-				} else {
+				}
+				else
+				{
 					return 3;
 				}
 			}
 
 			// Bind the data to the current user
-			if ($user->bind($data) == false) {
+			if ($user->bind($data) == false)
+			{
 				return false;
 			}
 
 			// Save the user
-			if ($user->save() == false) {
+			if ($user->save() == false)
+			{
 				return false;
 			}
 
 			// Save the user through the Profile Connectors
 			$profile = new MageBridgeConnectorProfile();
 			$profile->onSave($user, $customer, $address);
+
+		    // Run the newsletter plugins
+            if (isset($customer['is_subscribed']))
+            {
+        		JPluginHelper::importPlugin('magebridgenewsletter');
+		        JFactory::getApplication()->triggerEvent('onNewsletterSubscribe', array($user, (bool)$customer['is_subscribed']));
+            }
 
 			// Return the user ID for convenience
 			return $user->id;
@@ -290,9 +336,9 @@ class plgMagentoMageBridge extends JPlugin
 
 	/**
 	 * Handle the event that is generated after an order is saved in Magento (JSON-RPC)
-	 * 
-	 * @access public
+	 *
 	 * @param array $arguments
+	 *
 	 * @return bool
 	 */
 	public function mageCheckoutOnepageControllerSuccessAction($arguments = array())
@@ -303,7 +349,8 @@ class plgMagentoMageBridge extends JPlugin
 		$orderState = (isset($arguments['order']['state'])) ? $arguments['order']['state'] : null;
 
 		// Don't continue if this is a guest-only order
-		if (!isset($customer['email'])) {
+		if (!isset($customer['email']))
+		{
 			return false;
 		}
 
@@ -311,10 +358,16 @@ class plgMagentoMageBridge extends JPlugin
 		$user = $this->getUser()->loadByEmail($customer['email']);
 
 		// Loop through the products and run the product connector
-		foreach ($products as $product) {
+		foreach ($products as $product)
+		{
 			$productQty = (isset($product['qty'])) ? $product['qty'] : 1;
-			if ($productQty < 1) $productQty = 1; 
-			$rt = MageBridgeConnectorProduct::getInstance()->runOnPurchase($product['sku'], $productQty, $user, $orderState, $arguments);
+
+			if ($productQty < 1)
+			{
+				$productQty = 1;
+			}
+
+			MageBridgeConnectorProduct::getInstance()->runOnPurchase($product['sku'], $productQty, $user, $orderState, $arguments);
 		}
 
 		return true;
@@ -322,9 +375,9 @@ class plgMagentoMageBridge extends JPlugin
 
 	/**
 	 * Handle the event that is generated after a newsletter subscriber is changed in Magento
-	 * 
-	 * @access public
+	 *
 	 * @param array $arguments
+	 *
 	 * @return bool
 	 */
 	public function mageNewsletterSubscriberChangeAfter($arguments = array())
@@ -345,9 +398,9 @@ class plgMagentoMageBridge extends JPlugin
 
 	/**
 	 * Handle the event that is generated after an order is saved in Magento (RPC)
-	 * 
-	 * @access public
+	 *
 	 * @param array $arguments
+	 *
 	 * @return bool
 	 */
 	public function mageSalesOrderSaveAfter($arguments = array())
@@ -356,14 +409,18 @@ class plgMagentoMageBridge extends JPlugin
 		$products = $arguments['order']['products'];
 		$customer = $arguments['order']['customer'];
 
-		if (isset($arguments['order']['state'])) {
+		if (isset($arguments['order']['state']))
+		{
 			$state = $arguments['order']['state'];
-		} else {
+		}
+		else
+		{
 			$state = null;
 		}
 
 		// Don't continue if this is a guest-only order
-		if (!isset($customer['email'])) {
+		if (!isset($customer['email']))
+		{
 			return false;
 		}
 
@@ -371,10 +428,16 @@ class plgMagentoMageBridge extends JPlugin
 		$user = $this->getUser()->loadByEmail($customer['email']);
 
 		// Loop through the products and run the product connector
-		foreach ($products as $product) {
+		foreach ($products as $product)
+		{
 			$productQty = (isset($product['qty'])) ? $product['qty'] : 1;
-			if ($productQty < 1) $productQty = 1; 
-			$rt = MageBridgeConnectorProduct::getInstance()->runOnPurchase($product['sku'], $productQty, $user, $state, $arguments);
+
+			if ($productQty < 1)
+			{
+				$productQty = 1;
+			}
+
+			MageBridgeConnectorProduct::getInstance()->runOnPurchase($product['sku'], $productQty, $user, $state, $arguments);
 		}
 
 		return true;
@@ -382,9 +445,9 @@ class plgMagentoMageBridge extends JPlugin
 
 	/**
 	 * Handle the event that is generated after an order is completed in Magento (RPC)
-	 * 
-	 * @access public
+	 *
 	 * @param array $arguments
+	 *
 	 * @return bool
 	 */
 	public function mageSalesOrderCompleteAfter($arguments = array())
@@ -395,7 +458,8 @@ class plgMagentoMageBridge extends JPlugin
 		$state = $arguments['order']['state'];
 
 		// Don't continue if this is a guest-only order
-		if (!isset($customer['email'])) {
+		if (!isset($customer['email']))
+		{
 			return false;
 		}
 
@@ -403,10 +467,16 @@ class plgMagentoMageBridge extends JPlugin
 		$user = $this->getUser()->loadByEmail($customer['email']);
 
 		// Loop through the products and run the product connector
-		foreach ($products as $product) {
+		foreach ($products as $product)
+		{
 			$productQty = (isset($product['qty'])) ? $product['qty'] : 1;
-			if ($productQty < 1) $productQty = 1; 
-			$rt = MageBridgeConnectorProduct::getInstance()->runOnPurchase($product['sku'], $productQty, $user, $state, $arguments);
+
+			if ($productQty < 1)
+			{
+				$productQty = 1;
+			}
+
+			MageBridgeConnectorProduct::getInstance()->runOnPurchase($product['sku'], $productQty, $user, $state, $arguments);
 		}
 
 		return true;
@@ -414,9 +484,9 @@ class plgMagentoMageBridge extends JPlugin
 
 	/**
 	 * Return a MageBridge configuration parameter
-	 * 
-	 * @access private
+	 *
 	 * @param string $name
+	 *
 	 * @return mixed $value
 	 */
 	private function getParam($name = null)
@@ -426,49 +496,65 @@ class plgMagentoMageBridge extends JPlugin
 
 	/**
 	 * Helper function to determine the right username
-	 * 
-	 * @access private
-	 * @param array $arguments
+	 *
+	 * @param mixed $user
+	 * @param array $customer
+	 *
 	 * @return string
 	 */
 	private function getUsername($user, $customer)
 	{
 		// If Magento "magically" comes up with an username, use that (for instance, in case of a third party extension)
-		if (isset($customer['username'])) {
+		if (isset($customer['username']))
+		{
 			return $customer['username'];
 		}
 
 		// Do some checks, but only if $user is a valid object
-		if (is_object($user)) {
-			if ($this->getParam('username_from_email') == 1 || $user->get('username') == $user->get('email')) {
+		if (is_object($user))
+		{
+			if ($this->getParam('username_from_email') == 1 || $user->get('username') == $user->get('email'))
+			{
 				return $customer['email'];
-			} else {
+			}
+			else
+			{
 				return $user->get('username');
 			}
 
-		// Just use the email-address
-		} else {
+			// Just use the email-address
+		}
+		else
+		{
 			return $customer['email'];
 		}
 	}
 
 	/**
 	 * Helper function to determine the right name
-	 * 
-	 * @access private
-	 * @param array $arguments
+	 *
+	 * @param mixed $user
+	 * @param array $customer
+	 *
 	 * @return string
 	 */
 	private function getRealname($user, $customer)
 	{
-		if ($this->getParam('realname_from_firstlast') == 0 && is_object($user)) {
+		if ($this->getParam('realname_from_firstlast') == 0 && is_object($user))
+		{
 			return $user->get('name');
 		}
-		
-		if (isset($customer['firstname']) && isset($customer['lastname'])) {
-			return $customer['firstname'].' '.$customer['lastname'];
-		} else if (isset($customer['name'])) {
-			return $customer['name'];
+
+		if (isset($customer['firstname']) && isset($customer['lastname']))
+		{
+			return $customer['firstname'] . ' ' . $customer['lastname'];
+		}
+		else
+		{
+			if (isset($customer['name']))
+			{
+				return $customer['name'];
+			}
 		}
 
 		return null;
