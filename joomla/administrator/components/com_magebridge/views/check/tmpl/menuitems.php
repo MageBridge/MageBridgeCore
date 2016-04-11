7bb6ac15abbd09f8070cfd06563f538d31458c1a
Jisse Reitsma <info@yireo.com>
2016-04-11 17:39:48 +0200

    New layout to show all MageBridge Root Menu-Items for troubleshooting

---
+++ joomla/administrator/components/com_magebridge/views/check/tmpl/menuitems.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Joomla! component MageBridge
+ *
+ * @author Yireo (info@yireo.com)
+ * @package MageBridge
+ * @copyright Copyright 2015
+ * @license GNU Public License
+ * @link http://www.yireo.com
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+$items = MageBridgeUrlHelper::getRootItems();
+?>
+<h3>MageBridge Root Menu-Items</h3>
+<table class="table table-striped">
+	<thead>
+	<tr>
+		<th>
+			ID
+		</th>
+		<th>
+			Title
+		</th>
+		<th>
+			Menu
+		</th>
+	</tr>
+	</thead>
+	<tbody>
+	<?php foreach($items as $item): ?>
+	<tr>
+		<td>
+			<?php echo $item->id; ?>
+		</td>
+		<td>
+			<?php echo $item->title; ?>
+		</td>
+		<td>
+			<?php echo $item->menutype; ?>
+		</td>
+	</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
