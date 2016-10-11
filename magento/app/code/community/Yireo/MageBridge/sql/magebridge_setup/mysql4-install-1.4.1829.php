090b016484847a2d827ca478b06ac2d83ecc3f76
Jisse Reitsma <info@yireo.com>
2016-10-11 09:38:46 +0200

    Fix table creation issue

--- magento/app/code/community/Yireo/MageBridge/sql/magebridge_setup/mysql4-install-1.4.1829.php
+++ magento/app/code/community/Yireo/MageBridge/sql/magebridge_setup/mysql4-install-1.4.1829.php
@@ -13,9 +13,9 @@ $installer = $this;
 $installer->startSetup();
 $installer->run("
 CREATE TABLE IF NOT EXISTS `{$installer->getTable('magebridge_customer_joomla')}` (
-    `customer_id` int(10) unsigned default NULL,
-    `joomla_id` int(10) unsigned default NULL,
-    `website_id` int(10) unsigned default NULL,
+    `customer_id` int(10) UNSIGNED DEFAULT '0' NOT NULL,
+    `joomla_id` int(10) UNSIGNED DEFAULT '0' NOT NULL,
+    `website_id` int(10) UNSIGNED DEFAULT '0' NOT NULL,
     PRIMARY KEY  (`customer_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Relation between Joomla user and Magento customer';
 ");
