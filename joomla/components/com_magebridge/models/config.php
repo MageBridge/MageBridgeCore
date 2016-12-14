ecbad397e04ef1a4416491b34def4695da04fc50
Jisse Reitsma <info@yireo.com>
2016-12-14 19:13:41 +0100

    Optimize configuration handling

--- joomla/components/com_magebridge/models/config.php
+++ joomla/components/com_magebridge/models/config.php
@@ -14,6 +14,7 @@ defined('_JEXEC') or die('Restricted access');
 
 // Require the parent view
 require_once JPATH_SITE . '/components/com_magebridge/helpers/loader.php';
+require_once JPATH_SITE . '/components/com_magebridge/models/config/value.php';
 
 /**
  * Bridge configuration class
@@ -25,14 +26,19 @@ class MagebridgeModelConfig extends YireoAbstractModel
 	 *
 	 * @var array
 	 */
-	protected $_data = null;
+	protected $data = null;
+
+	/**
+	 * @var array
+	 */
+	protected $config = [];
 
 	/**
 	 * Array of default values
 	 *
 	 * @var array
 	 */
-	protected $_defaults = null;
+	protected $defaults = null;
 
 	/**
 	 * Constructor
@@ -43,7 +49,7 @@ class MagebridgeModelConfig extends YireoAbstractModel
 	 */
 	public function __construct()
 	{
-		$this->_defaults = (new MagebridgeModelConfigDefaults)->getDefaults();
+		$this->defaults = (new MageBridgeModelConfigDefaults)->getDefaults();
 
 		parent::__construct();
 	}
@@ -59,7 +65,7 @@ class MagebridgeModelConfig extends YireoAbstractModel
 
 		if ($instance === null)
 		{
-			$instance = new MageBridgeModelConfig();
+			$instance = new MageBridgeModelConfig;
 		}
 
 		return $instance;
@@ -74,7 +80,7 @@ class MagebridgeModelConfig extends YireoAbstractModel
 	 */
 	public function setData($data)
 	{
-		$this->_data = $data;
+		$this->data = $data;
 	}
 
 	/**
@@ -85,17 +91,17 @@ class MagebridgeModelConfig extends YireoAbstractModel
 	public function getData()
 	{
 		// Lets load the content if it doesn't already exist
-		if (empty($this->_data))
+		if (empty($this->data))
 		{
 			$query = $this->_db->getQuery(true);
-			$query->select($this->_db->quoteName(['id', 'name']));
+			$query->select($this->_db->quoteName(['id', 'name', 'value']));
 			$query->from($this->_db->quoteName('#__magebridge_config', 'c'));
 
 			$this->_db->setQuery($query);
-			$this->_data = $this->_db->loadObjectList();
+			$this->data = $this->_db->loadObjectList();
 		}
 
-		return $this->_data;
+		return $this->data;
 	}
 
 	/**
@@ -105,154 +111,172 @@ class MagebridgeModelConfig extends YireoAbstractModel
 	 */
 	public function getDefaults()
 	{
-		return $this->_defaults;
+		return $this->defaults;
+	}
+
+	/**
+	 * @return array
+	 */
+	private function loadDefaultConfig()
+	{
+		foreach ($this->getDefaults() as $name => $value)
+		{
+			$this->config[$name] = (new MagebridgeModelConfigValue(['name' => $name, 'value' => $value]))->toArray();
+		}
+
+		return $this->config;
+	}
+
+	/**
+	 * @param $data
+	 *
+	 * @return array
+	 */
+	private function pushDataIntoConfig($data)
+	{
+		foreach ($this->config as $name => $c)
+		{
+			if (!empty($data))
+			{
+				foreach ($data as $d)
+				{
+					if ($d->name == $c['name'])
+					{
+						$d->isOriginal  = ($this->config[$name]['value'] == $d->value) ? 1 : 0;
+						$d->description = $c['description'];
+
+						$this->config[$name] = (new MagebridgeModelConfigValue((array) $d))->toArray();
+						break;
+					}
+				}
+			}
+		}
+
+		return $this->config;
+	}
+
+	private function overrideConfig()
+	{
+		$this->config['method'] = (new MagebridgeModelConfigValue(['value' => 'post']))->toArray();
+
+		// Determine the right update format
+		if ($this->config['update_format']['value'] == '')
+		{
+			jimport('joomla.application.component.helper');
+			$component = JComponentHelper::getComponent('com_magebridge');
+			$params    = YireoHelper::toRegistry($component->params);
+			$value     = $params->get('update_format', 'tar.gz');
+
+			$this->config['update_format'] = (new MagebridgeModelConfigValue(['value' => $value]))->toArray();
+		}
+
+		// Disable widgets if needed
+		if ($this->input->getInt('widgets', 1) == 0)
+		{
+			$this->config['api_widgets'] = (new MagebridgeModelConfigValue(['value' => 0]))->toArray();
+		}
+
+		// Overload a certain values when the Magento Admin Panel needs to be loaded
+		if ($this->app->isAdmin() && $this->input->getCmd('option') == 'com_magebridge' && $this->input->getCmd('view') == 'root')
+		{
+			//$this->config['debug'] = (new MagebridgeModelConfigValue(['value' => 0]))->toArray();
+			$this->config['disable_js_all'] = (new MagebridgeModelConfigValue(['value' => 1]))->toArray();
+			$this->config['disable_js_mootools'] = (new MagebridgeModelConfigValue(['value' => 1]))->toArray();
+		}
+
+		// Return the URL
+		if (!isset($this->config['url']))
+		{
+			$url = '';
+
+			if (!empty($this->config['host']['value']))
+			{
+				$url = $this->config['protocol']['value'] . '://' . $this->config['host']['value'] . '/';
+
+				if (!empty($this->config['basedir']['value']))
+				{
+					$url .= $this->config['basedir']['value'] . '/';
+				}
+			}
+
+			$this->config['url'] = (new MagebridgeModelConfigValue(['value' => $url]))->toArray();
+		}
+
+		// Return the port-number
+		if (!isset($this->config['port']))
+		{
+			$value = ($this->config['protocol']['value'] == 'http') ? 80 : 443;
+			$this->config['port'] = (new MagebridgeModelConfigValue(['value' => $value]))->toArray();
+		}
+
+		return $this->config;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getConfiguration()
+	{
+		if (empty($this->config))
+		{
+			$this->loadDefaultConfig();
+			$this->pushDataIntoConfig($this->getData());
+			$this->overrideConfig();
+		}
+
+		return $this->config;
+	}
+
+	public function setConfigValue($name, $value)
+	{
+		$this->config[$name]['value'] = $value;
 	}
 
 	/**
 	 * Static method to get data
 	 *
 	 * @param string $element
-	 * @param
+	 * @param mixed  $overloadValue
 	 *
 	 * @return mixed
 	 */
-	static public function load($element = null, $overload = null)
+	static public function load($element = null, $overloadValue = null)
 	{
-		$application = JFactory::getApplication();
-
 		static $config = null;
+		$configModel = self::getSingleton();
+
 		if (empty($config))
 		{
-
-			// Parse the defaults
-			$config = array();
-			$model  = self::getSingleton();
-			foreach ($model->getDefaults() as $name => $value)
-			{
-				$config[$name] = array(
-					'id'          => null,
-					'name'        => $name,
-					'value'       => $value,
-					'core'        => 1,
-					'description' => null,
-				);
-
-				if ($application->isAdmin())
-				{
-					$config[$name]['description'] = JText::_(strtoupper($name) . '_DESCRIPTION');
-				}
-			}
-
-			// Fetch the current data
-			$data = $model->getData();
-
-			// Parse the current data into the config
-			foreach ($config as $name => $c)
-			{
-				if (!empty($data))
-				{
-					foreach ($data as $d)
-					{
-						if ($d->name == $c['name'])
-						{
-							$core          = ($config[$name]['value'] == $d->value) ? 1 : 0;
-							$config[$name] = array(
-								'id'          => $d->id,
-								'name'        => $d->name,
-								'value'       => $d->value,
-								'core'        => $core,
-								'description' => $c['description'],
-							);
-							break;
-						}
-					}
-				}
-			}
-		}
-
-		// Override certain values
-		$config['method']['value'] = 'post';
-
-		// Determine the right update format
-		if ($config['update_format']['value'] == '')
-		{
-
-			jimport('joomla.application.component.helper');
-			$component = JComponentHelper::getComponent('com_magebridge');
-			$params = YireoHelper::toRegistry($component->params);
-
-			$value                            = $params->get('update_format', 'tar.gz');
-			$config['update_format']['value'] = $value;
-		}
-
-		// Disable widgets if needed
-		if (JFactory::getApplication()->input->getInt('widgets', 1) == 0)
-		{
-			$config['api_widgets']['value'] = 0;
-		}
-
-		// Overload a certain values when the Magento Admin Panel needs to be loaded
-		if ($application->isAdmin() && $application->input->getCmd('option') == 'com_magebridge' && $application->input->getCmd('view') == 'root')
-		{
-			//$config['debug']['value'] = 0;
-			$config['disable_js_all']['value']      = 1;
-			$config['disable_js_mootools']['value'] = 1;
+			$config = ($configModel)->getConfiguration();
 		}
 
 		// Allow overriding values
-		if (!empty($element) && isset($config[$element]) && $overload !== null)
+		if (!empty($element) && isset($config[$element]) && $overloadValue !== null)
 		{
-			$config[$element]['value'] = $overload;
+			$configModel->setConfigValue($element, $overloadValue);
+
+			return $overloadValue;
 		}
 
-		// Return the URL
-		if ($element == 'url')
+		// Return any other element
+		if ($element != null && isset($config[$element]))
 		{
-			$url = null;
-			if (!empty($config['host']['value']))
+			if (!isset($config[$element]['value']))
 			{
-				$url = $config['protocol']['value'] . '://' . $config['host']['value'] . '/';
-				if (!empty($config['basedir']['value']))
-				{
-					$url .= $config['basedir']['value'] . '/';
-				}
+				print_r($config[$element]);
 			}
 
-			return $url;
-
-			// Return the port-number
+			return $config[$element]['value'];
 		}
-		else
+
+		// Return no value
+		if (!empty($element))
 		{
-			if ($element == 'port')
-			{
-				return ($config['protocol']['value'] == 'http') ? 80 : 443;
-
-				// Return any other element
-			}
-			else
-			{
-				if ($element != null && isset($config[$element]))
-				{
-					return $config[$element]['value'];
-
-					// Return no value
-				}
-				else
-				{
-					if (!empty($element))
-					{
-						return null;
-
-						// Return the configuration itself
-					}
-					else
-					{
-						return $config;
-					}
-				}
-			}
+			return null;
 		}
+
+		// Return the configuration itself
+		return $config;
 	}
 
 	/**
@@ -284,29 +308,23 @@ class MagebridgeModelConfig extends YireoAbstractModel
 			if (preg_match('/([^a-zA-Z0-9\.\-\_\:]+)/', $value) == true)
 			{
 				return JText::_('Hostname contains illegal characters. Note that a hostname is not an URL, but only a fully qualified domainname.');
-
 			}
-			else
+
+			if (gethostbyname($value) == $value && !preg_match('/([0-9\.]+)/', $value))
 			{
-				if (gethostbyname($value) == $value && !preg_match('/([0-9\.]+)/', $value))
+				return JText::sprintf('DNS lookup of hostname %s failed', $value);
+			}
+
+			if (MagebridgeModelConfig::load('api_widgets') == true)
+			{
+				$bridge = MageBridgeModelBridge::getInstance();
+				$data   = $bridge->build();
+
+				if (empty($data))
 				{
-					return JText::sprintf('DNS lookup of hostname %s failed', $value);
+					$url = $bridge->getMagentoBridgeUrl();
 
-				}
-				else
-				{
-					if (MagebridgeModelConfig::load('api_widgets') == true)
-					{
-
-						$bridge = MageBridgeModelBridge::getInstance();
-						$data   = $bridge->build();
-						if (empty($data))
-						{
-							$url = $bridge->getMagentoBridgeUrl();
-
-							return JText::sprintf('Unable to open a connection to <a href="%s" target="_new">%s</a>', $url, $url);
-						}
-					}
+					return JText::sprintf('Unable to open a connection to <a href="%s" target="_new">%s</a>', $url, $url);
 				}
 			}
 		}
@@ -369,31 +387,31 @@ class MagebridgeModelConfig extends YireoAbstractModel
 		 */
 
 		// Check basedir
-		if ($element == 'basedir')
+		if ($element !== 'basedir')
 		{
-			if (empty($value))
-			{
-				return null;
-			}
-
-			if (preg_match('/([a-zA-Z0-9\.\-\_]+)/', $value) == false)
-			{
-				return JText::_('Basedir contains illegal characters');
-			}
-
-			$root         = MageBridgeUrlHelper::getRootItem();
-			$joomla_host  = JFactory::getURI()
-				->toString(array('host'));
-			$magento_host = MagebridgeModelConfig::load('host');
-
-			// Check whether the Magento basedir conflicts with the MageBridge alias
-			if (!empty($root) && !empty($root->route) && $root->route == $value && $joomla_host == $magento_host)
-			{
-				return JText::_('Magento basedir is same as MageBridge alias, which is not possible');
-			}
+			return null;
 		}
 
-		return null;
+		if (empty($value))
+		{
+			return null;
+		}
+
+		if (preg_match('/([a-zA-Z0-9\.\-\_]+)/', $value) == false)
+		{
+			return JText::_('Basedir contains illegal characters');
+		}
+
+		$root         = MageBridgeUrlHelper::getRootItem();
+		$joomla_host  = JUri::getInstance()
+			->toString(array('host'));
+		$magento_host = MagebridgeModelConfig::load('host');
+
+		// Check whether the Magento basedir conflicts with the MageBridge alias
+		if (!empty($root) && !empty($root->route) && $root->route == $value && $joomla_host == $magento_host)
+		{
+			return JText::_('Magento basedir is same as MageBridge alias, which is not possible');
+		}
 	}
 
 	/**
@@ -405,22 +423,23 @@ class MagebridgeModelConfig extends YireoAbstractModel
 	 */
 	static public function allEmpty()
 	{
-		static $_allempty = null;
-		if (empty($_allempty))
+		static $allEmpty = null;
+
+		if (empty($allEmpty))
 		{
-			$_allempty = true;
-			$config    = MagebridgeModelConfig::load();
+			$allEmpty = true;
+			$config   = MagebridgeModelConfig::load();
 			foreach ($config as $c)
 			{
 				if ($c['core'] == 0)
 				{
-					$_allempty = false;
+					$allEmpty = false;
 					break;
 				}
 			}
 		}
 
-		return $_allempty;
+		return $allEmpty;
 	}
 
 	/**
@@ -429,6 +448,7 @@ class MagebridgeModelConfig extends YireoAbstractModel
 	 * @param array $post
 	 *
 	 * @return bool
+	 * @throws Exception
 	 */
 	public function store($post)
 	{
@@ -490,13 +510,13 @@ class MagebridgeModelConfig extends YireoAbstractModel
 		}
 
 		// Check whether the URL-table contains entries
-		$db = JFactory::getDbo();
+		$db    = JFactory::getDbo();
 		$query = $db->getQuery(true);
 		$query->select('*');
 		$query->from($db->quoteName('#__magebridge_urls'));
 		$query->where($db->quoteName('published') . ' = 1');
 		$rows = $db->loadObjectList();
-		
+
 		if (!empty($rows))
 		{
 			$post['load_urls'] = 1;
@@ -507,13 +527,13 @@ class MagebridgeModelConfig extends YireoAbstractModel
 		}
 
 		// Check whether the stores-table contains entries
-		$db = JFactory::getDbo();
+		$db    = JFactory::getDbo();
 		$query = $db->getQuery(true);
 		$query->select('*');
 		$query->from($db->quoteName('#__magebridge_stores'));
 		$query->where($db->quoteName('published') . ' = 1');
 		$rows = $db->loadObjectList();
-		
+
 		if (!empty($rows))
 		{
 			$post['load_stores'] = 1;
@@ -536,19 +556,19 @@ class MagebridgeModelConfig extends YireoAbstractModel
 		}
 
 		// Detect changes in API-settings and if so, dump and clean the cache
-		$detect_values = array('host', 'basedir', 'api_user', 'api_password');
-		$detect_change = false;
-		
+		$detect_values  = array('host', 'basedir', 'api_user', 'api_password');
+		$changeDetected = false;
+
 		foreach ($detect_values as $d)
 		{
 			if (isset($post[$d]) && isset($config[$d]) && $post[$d] != $config[$d])
 			{
-				$detect_change = true;
+				$changeDetected = true;
 			}
 		}
 
 		// Clean the cache if changes are detected
-		if ($detect_change)
+		if ($changeDetected)
 		{
 			/** @var JCache $cache */
 			$cache = JFactory::getCache('com_magebridge.admin');
@@ -564,7 +584,7 @@ class MagebridgeModelConfig extends YireoAbstractModel
 				continue;
 			}
 
-    		$table = JTable::getInstance('config', 'MagebridgeTable');
+			$table = JTable::getInstance('config', 'MagebridgeTable');
 
 			if (!$table->bind($data))
 			{
@@ -584,7 +604,7 @@ class MagebridgeModelConfig extends YireoAbstractModel
 	 * Method to store a single value in the database
 	 *
 	 * @param string $name
-	 * @param mixed $value
+	 * @param mixed  $value
 	 *
 	 * @return bool
 	 * @throws Exception
