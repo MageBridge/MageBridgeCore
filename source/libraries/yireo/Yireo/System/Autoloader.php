a58f2f525200104af7455565a9de242cf9458c30
Jisse Reitsma <info@yireo.com>
2017-02-17 18:54:16 +0100

    Add YireoFormFieldPublished to autoloader

--- source/libraries/yireo/Yireo/System/Autoloader.php
+++ source/libraries/yireo/Yireo/System/Autoloader.php
@@ -35,6 +35,7 @@ class Autoloader
 		'YireoAbstractView'       => 'view/abstract',
 		'YireoCommonController'   => 'controller/common',
 		'YireoAbstractController' => 'controller/abstract',
+        'YireoFormFieldPublished' => 'form/fields/published',
 	);
 
 	/**
